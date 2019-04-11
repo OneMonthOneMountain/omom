36dc0da977970efefa01a135c14066f198ac1518
Tom Lloyd <tom.w.lloyd@googlemail.com>
2019-04-11 19:15:20 +0100

    fixed failing test

--- website/src/App.js
+++ website/src/App.js
@@ -5,36 +5,39 @@ import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
 import { LinkContainer } from 'react-router-bootstrap'
 import Routes from './Routes'
+import { BrowserRouter as Router } from "react-router-dom"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar collapseOnSelect variant={"dark"} expand={"md"} bg={"dark"}>
-          <LinkContainer to="/">
-            <Image roundedCircle className={"Icon"} src={"omom.png"}/>
-          </LinkContainer>
-          
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Nav>
-              <LinkContainer to="/upcoming">
-                <Nav.Link>Upcoming</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/completed">
-                <Nav.Link>Completed</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/suggested">
-                <Nav.Link>Suggested</Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Container>
-          <Routes />
-        </Container>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar collapseOnSelect variant={"dark"} expand={"md"} bg={"dark"}>
+            <LinkContainer to="/">
+              <Image roundedCircle className={"Icon"} src={"omom.png"}/>
+            </LinkContainer>
+            
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Nav>
+                <LinkContainer to="/upcoming">
+                  <Nav.Link>Upcoming</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/completed">
+                  <Nav.Link>Completed</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/suggested">
+                  <Nav.Link>Suggested</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Container>
+            <Routes />
+          </Container>
+        </div>
+      </Router>
     );
   }
 }
